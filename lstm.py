ce5e84f7bf8a86e2109403361a4e23e2f6ee7086
nobuyuki-kawagashira <nobuyuki_kawagashira@waku-2.com>
2021-08-13 16:51:44 +0900

    write parameter file

--- lstm.py
+++ lstm.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 from keras.models import Sequential
+from keras.models import load_model
 from keras.layers import Dense, LSTM, Activation
 from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras.callbacks import EarlyStopping
@@ -17,12 +18,18 @@ def train(x, y):
     # 1つの学習データのStep数(今回は25)
     length_of_sequence = X.shape[1] 
     in_out_neurons = 1
-    n_hidden = 300
-    #n_hidden = 100
-    #n_hidden = 30
-    #n_hidden = 10
 
     model = Sequential()
+    """
+    model.add(LSTM(
+        n_hidden,
+        #dropout=0.5,
+        #recurrent_dropout=0.5,
+        #kernel_regularizer=l2(0.01),
+        #bias_regularizer=l2(0.001),
+        batch_input_shape=(None, length_of_sequence, in_out_neurons),
+        return_sequences=True))
+    """
     model.add(LSTM(
         n_hidden,
         #dropout=0.5,
@@ -42,7 +49,7 @@ def train(x, y):
     early_stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=20)
     model.fit(x, y,
           batch_size=100,
-          epochs=3,
+          epochs=n_epoch,
           validation_split=0.3,
           callbacks=[early_stopping]
           )
@@ -61,11 +68,10 @@ def predict_future(model, X, length):
         future_test = np.append(future_test, batch_predict)
         future_result = np.append(future_result, batch_predict)
     future_result = np.delete(future_result, 0)
-    #print (future_result.reshape(future_result.shape[0], 1))
     return future_result.reshape(future_result.shape[0], 1)
 
 
-def plot(y, pred, d):
+def plot(y, pred, d, f):
 
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
@@ -88,33 +94,72 @@ def plot(y, pred, d):
     plt.plot(pred, label='#predicted')
     plt.ylabel('#persons (x 10000)')
     plt.legend()
-    plt.show()
+    #plt.show()
+    print ('GRAPH', f)
+    plt.savefig(f)
+    plt.close()
+    return
 
 
 if __name__ == '__main__':
 
     import pickle
-    #train_size = 552
-    #train_size = 557
+    import json
+    import datetime
+    import os
+
+    n_hidden = 300
     train_size = 559
+    n_epoch = 3
+
+    now = datetime.datetime.now()
+    timecode = now.strftime("%y%m%d-%H%M")     # like 210813-0909
+    dir = 'model/' + timecode + '/'
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+    param = {}
+    param['train_size'] = train_size
 
     #with open('covid19-7day.pkl', 'rb') as i_handle:
     #with open('covid19-14day.pkl', 'rb') as i_handle:
     #with open('covid19-14day-0809.pkl', 'rb') as i_handle:
-    with open('covid19-14day-0811.pkl', 'rb') as i_handle:
+    i_file = 'data/pkl/covid19-14day-0811.pkl'
+    with open(i_file, 'rb') as i_handle:
         X, X_log, y, d = pickle.load(i_handle)
     print ('X', X.shape)
     X_train, y_train = X[:train_size], y[:train_size]
     #X_train, y_train = X_log[:train_size], y[:train_size]
     model = train(X_train, y_train)
-    model.save('model.hdf5')
-    #print (model.__dict__)
-    #with open('model.pkl', 'wb') as m_handle:
-    #    pickle.dump(model, m_handle)
+    m_file = 'model/%s/%s.h5' % (timecode, timecode)
+    t_file = 'model/%s/%s-model.txt' % (timecode, timecode)
+    param['files'] = {}
+    param['files']['resource']      = i_file
+    param['files']['model']         = m_file
+    param['files']['model_desc']    = t_file
+
+    model.save(m_file)
+    print ('SAVED MODEL', m_file)
+    with open(t_file, 'w') as t_handle:
+        model.summary(print_fn=lambda x: t_handle.write(x + '\n'))
+    model = None
+    model = load_model(m_file)
+
+    param['optimizer'] = {}
+    param['optimizer']['name'] = model.optimizer._name
+    param['optimizer']['learning_rate'] = float(model.optimizer._hyper['learning_rate'])
+    p_file = dir + timecode + '-param.txt'
+    param['files']['parameter'] = p_file
+
     pred = model.predict(X_train)
     print (y_train.shape, pred.shape)
-    #plot(y, pred)
     future_result = predict_future(model, X_train, X.shape[0] - train_size + 90)
     con_pred = np.vstack([pred, future_result])
     print (y.shape, con_pred.shape)
-    plot(y, con_pred, d)
+    g_file = 'model/%s/%s-graph.png' % (timecode, timecode)
+    param['files']['analytical_graph'] = g_file
+    plot(y, con_pred, d, g_file)
+
+    print ('PARAM', p_file)
+    with open(p_file, 'w') as p_handle:
+        p_handle.write(json.dumps(param, indent=2))
